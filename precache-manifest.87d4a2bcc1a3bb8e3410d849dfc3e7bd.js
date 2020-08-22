1d7ff1f5f18f43cf940d271c333d7e75b234787f
Joshua Horwitz <jahorwitz94@gmail.com>
2020-08-21 21:18:02 -0400

    Updates

---
+++ precache-manifest.87d4a2bcc1a3bb8e3410d849dfc3e7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69426d1452fca3219d21922794e04504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c39dfad566549d02275",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1c39dfad566549d02275",
+    "url": "/static/js/2.cc27d151.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc27d151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769db8503a2952a195b3",
+    "url": "/static/js/main.c1d91728.chunk.js"
+  },
+  {
+    "revision": "20881236596aa410a6ca",
+    "url": "/static/js/runtime-main.5826ac29.js"
+  }
+]);
