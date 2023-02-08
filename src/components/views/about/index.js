9c825f70e62fd19127f7f289215e8890c781f79b
Matt Horwitz <mah9122@gmail.com>
2023-02-08 07:09:24 +0100

    updated bio

--- src/components/views/about/index.js
+++ src/components/views/about/index.js
@@ -91,6 +91,8 @@ export default () => {
                     Marie Skłodowska-Curie Individual Fellowship and a Junior
                     Research Fellowship at Kellogg College (University of
                     Oxford). In 2021, he became a Scientist at Janssen Pharmaceutical Companies of Johnson & Johnson in Chemical Process Research & Development (CPRD) in Belgium.
+
+He currently serves on the Advisory Board of Synthesis <a href=“https://www.thieme-connect.com/products/ejournals/journal/10.1055/s-00000084”>this is a link</a> and the Early Career Advisory Board of Tetrahedron <a href=“https://www.sciencedirect.com/journal/tetrahedron”>this is a link</a> and Tetrahedron Letters <a href=“https://www.sciencedirect.com/journal/tetrahedron-letters”>this is a link</a>.
                   </p>
                 </div>
               </div>
