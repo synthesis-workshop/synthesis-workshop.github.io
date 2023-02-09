2e3dc4ddf2e966ea6ee94ba3df9a26adea487d44
Josh Horwitz <jahorwitz94@gmail.com>
2023-02-08 22:45:26 -0500

    fixed about section links

--- src/components/views/about/index.js
+++ src/components/views/about/index.js
@@ -90,9 +90,22 @@ export default () => {
                     to work in the Gouverneur group. In 2020, he was awarded a
                     Marie Skłodowska-Curie Individual Fellowship and a Junior
                     Research Fellowship at Kellogg College (University of
-                    Oxford). In 2021, he became a Scientist at Janssen Pharmaceutical Companies of Johnson & Johnson in Chemical Process Research & Development (CPRD) in Belgium.
-
-He currently serves on the Advisory Board of Synthesis <a href=https://www.thieme-connect.com/products/ejournals/journal/10.1055/s-00000084>this is a link</a> and the Early Career Advisory Board of Tetrahedron <a href=https://www.sciencedirect.com/journal/tetrahedron>this is a link</a> and Tetrahedron Letters <a href=https://www.sciencedirect.com/journal/tetrahedron-letters>this is a link</a>.
+                    Oxford). In 2021, he became a Scientist at Janssen
+                    Pharmaceutical Companies of Johnson & Johnson in Chemical
+                    Process Research & Development (CPRD) in Belgium. He
+                    currently serves on the Advisory Board of{" "}
+                    <a href="https://www.thieme-connect.com/products/ejournals/journal/10.1055/s-00000084">
+                      Synthesis
+                    </a>{" "}
+                    and the Early Career Advisory Board of{" "}
+                    <a href="https://www.sciencedirect.com/journal/tetrahedron">
+                      Tetrahedron
+                    </a>{" "}
+                    and{" "}
+                    <a href="https://www.sciencedirect.com/journal/tetrahedron-letters">
+                      Tetrahedron Letters
+                    </a>
+                    .
                   </p>
                 </div>
               </div>
